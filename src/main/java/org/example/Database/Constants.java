bbe637e1e05f2bd05387115b4ce002158c9b62cc
Casper Daris <casperdaris@msn.com>
2020-12-21 12:51:50 +0100

    Constants toegevoegd

---
+++ src/main/java/org/example/Database/Constants.java
@@ -0,0 +1,15 @@
+package org.example.Database;
+
+// Dit bestand bevat gevoelige informatie, zorg dus dat je deze niet trackt!
+
+public class Constants {
+
+    static String CONN_URL = "jdbc:postgresql://rogue.db.elephantsql.com:5432/xwpagpbd";
+    static String CONN_USER = "xwpagpbd";
+    static String CONN_PASS = "4Cw0k9cOwiVOcrSuN5aZ0BNP28gLPFrJ";
+
+    static String TTN_REGION = "eu";
+    static String TTN_APP_ID = "smart_farm_gas_module";
+    static String TTN_ACCESS_KEY = "ttn-account-v2.WdvDE8Z7AwoUUf2kmC6TELjJNFM-W7qFCF36pmJDoac";
+
+}
